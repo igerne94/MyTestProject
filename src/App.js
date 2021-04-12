be4718a30f8c5c334cfeaeffada7e400803fbb83
Hanna <igerne94@gmail.com>
2021-04-12 19:51:19 +0200

    active page navigation

--- src/App.js
+++ src/App.js
@@ -3,23 +3,62 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NewPage } from './components/NewPage.jsx';
 import { HomePage } from './components/HomePage.jsx';
 import { Documentation } from './components/Documentation.jsx';
+import Nav from 'react-bootstrap/Nav'
+
+import { Tabs, Tab, ControlledTabs,  } from 'react-bootstrap';
 
 export const App = class App extends React.Component {
   
+  
   render() {
-    
+
     return (
       <div className="App">
+
         <Router>
           <div>
 
-            <nav>
+          
+{/*
+<nav>
+            
               <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                <a className="nav-item nav-link active" id="nav-homepage-tab" data-toggle="tab" href="/" role="tab" aria-controls="nav-homepage" aria-selected="true">Search HAPI</a>
-                <a className="nav-item nav-link" id="nav-newpage-tab" data-toggle="tab" href="/newpage" role="tab" aria-controls="nav-newpage" aria-selected="false">Get ID</a>
-                <a className="nav-item nav-link" id="nav-documentation-tab" data-toggle="tab" href="/documentation" role="tab" aria-controls="nav-documentation" aria-selected="false">Documentation</a>
+                <a className="nav-item nav-link active" id="homepage-tab" data-toggle="tab" href="/" role="tablist" aria-controls="nav-homepage" aria-selected="true" eventKey="homepage">Search HAPI</a>
+                <a className="nav-item nav-link" id="newpage-tab" data-toggle="tab" href="/newpage" role="tablist" aria-controls="nav-newpage" aria-selected="false" eventKey="newpage">Get ID</a>
+                <a className="nav-item nav-link" id="documentation-tab" data-toggle="tab" href="/documentation" role="tablist" aria-controls="nav-documentation" aria-selected="false" eventKey="documentation">Documentation</a>
+              
               </div>
+              
             </nav>
+
+*/}
+              
+            
+            {/*<nav>
+              <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+                <Tab className="nav-item nav-link active" id="homepage-tab" data-toggle="tab" href="/" role="tablist" aria-controls="nav-homepage" aria-selected="true" eventKey="homepage" >
+                 
+                </Tab>
+                <Tab eventKey="newpage" title="newpage">
+                  
+                </Tab>
+                <Tab eventKey="documentation" title="documentation">
+               
+                </Tab>
+              </Tabs>
+            </nav>*/}
+            
+            <Nav variant="tabs" defaultActiveKey={window.location.pathname}>
+              <Nav.Item>
+                <Nav.Link href="/">Home</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/newpage">New page</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="/documentation">Documentation</Nav.Link>
+              </Nav.Item>
+            </Nav>
 
             <Switch>
               <Route exact path="/" >

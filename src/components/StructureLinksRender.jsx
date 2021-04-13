58e0e5d74bb1ab05fb38c3d261d85f2e75f476fe
Eirik Hafver Rønjum <64041578+eirikconteir@users.noreply.github.com>
2021-04-13 22:09:45 +0200

    Comment out unused component in order to compile

--- src/components/StructureLinksRender.jsx
+++ src/components/StructureLinksRender.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 export const StructureLinksRender = class StructureLinksRender extends React.Component {
 

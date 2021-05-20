0ed608748f037e8746f712d7108ebac0dd58c6dc
Hanna <igerne94@gmail.com>
2021-05-20 22:23:35 +0200

    Unfinished 2

--- src/components/Record.jsx
+++ src/components/Record.jsx
@@ -25,40 +25,77 @@ export const Record = class Record extends React.Component {
         return promise;
       }
      
-      handleClick = () => {
+    handleClick = () => {
+        let promises = [];
+        let content = {};
+
           // ICPC2
         let codeSystemUrl1 = 'https://snowstorm.rundberg.no/browser/MAIN/ICPC2/members'
         + '?limit=10'
         + '&active=true'
         + '&referenceSet=450993002'
         + '&referencedComponentId=' + 35489007;
-        fetch(codeSystemUrl1)
+
+        let promiseICPC2 = fetch(codeSystemUrl1)
             .then(response => response.json())
             .then(data => {
-                
                 console.log(data);
+                if(data && Array.isArray(data.items) && data.items.length > 0) {
+                    if(data.items[0]?.additionalFields?.mapTarget) {
+                        content.icpc2 = {
+                            code: data.items[0]?.additionalFields?.mapTarget
+                        };
+                    }
+                }  
             });
+        promises.push(promiseICPC2);
             
-
             // ICD
-            let codeSystemUrl = 'https://snowstorm.conteir.no/browser/MAIN/members'
+        let codeSystemUrl = 'https://snowstorm.conteir.no/browser/MAIN/members'
         + '?limit=10'
         + '&active=true'
         + '&referenceSet=447562003'
         + '&referencedComponentId=' + 35489007;
-        fetch(codeSystemUrl)
+
+        let promiseICD10 = fetch(codeSystemUrl)
             .then(response => response.json())
             .then(data => {
-                
-                console.log(data);
+                console.log('icd10',data);
+                if(data && Array.isArray(data.items) && data.items.length > 0) {
+                    if(data.items[0]?.additionalFields?.mapTarget) {
+                        content.icd10 = {
+                            code: data.items[0]?.additionalFields?.mapTarget
+                        };
+                    }
+                } 
             });
-            
+        promises.push(promiseICD10);
+
+        Promise.all(promises).then(() => {
+            let contentPromises = [];
+            // Fetch by ICPC2 if available
+            if(content.icpc2) {
+                let promiseICPC2Content = fetch().then().then();
+                contentPromises.push(promiseICPC2Content);
+            }
+
+            if(content.icd10) {
+                let promiseICD10Content = fetch().then().then();
+                contentPromises.push(promiseICD10Content);
+            }
+
+            Promise.all(contentPromises).then(() => {
+                //render here
+            });
+        });
+
         let codeSystemUrl3 = 'https://api.helsedirektoratet.no/innhold/'
         + 'innhold'
         + '?kodeverk='
         + 'ICPC-2'
         + '&kode='
         + 'p76';
+
         fetch(codeSystemUrl3,
             {
                 method: 'GET',
@@ -69,11 +106,10 @@ export const Record = class Record extends React.Component {
               })
             .then(response => response.json())
             .then(data => {
-                
-                console.log(data);
+                console.log("this is just data", data);
             });
 
-        }
+    }
       
 
     render() {
